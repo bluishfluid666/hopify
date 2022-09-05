aa4080e8be0198eddb9608e455279b94f9f0ad06
Khang Manh Nguyen <nguyenmanhkhang0805@gmail.com>
2022-09-05 16:10:42 +0700

    Modeling users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/new'
   get '/help', to: "static_pages#help"
   get '/blog', to: "static_pages#blog"
   get '/contact', to: "static_pages#contact"
+  get '/signup', to: "users#new"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :user
   # Defines the root path route ("/")
   root "static_pages#home"
 end
