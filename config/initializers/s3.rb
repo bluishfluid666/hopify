e33fcbcc5131dec041340ed103f5263c04de92a9
Khang Manh Nguyen <nguyenmanhkhang0805@gmail.com>
2022-09-08 17:23:46 +0700

    add storj s3 provider config for avatar upload on production

---
+++ config/initializers/s3.rb
@@ -0,0 +1,10 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => "jutnxoxwwvzf266il65igf3fnjoq",
+      :aws_secret_access_key  => "j3g5j3siqyclxefn7nvbgv4vthgfslkht23udjvsknh4imtff3pi2",
+      :region                 => 'us-east-1' # Change this for different AWS region. Default is 'us-east-1'
+      :endpoint                 => 'https://gateway.storjshare.io' # Change this for different AWS region. Default is 'us-east-1'
+  }
+  config.fog_directory  = "hopify-bucket"
+end
