6d9108fbc38733b920d66b6882eedb1429deb0f7
Khang Manh Nguyen <nguyenmanhkhang0805@gmail.com>
2022-09-08 17:26:44 +0700

    Added comma between key-value pairs in initializers/s3.rb

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|
       :provider               => 'AWS',
       :aws_access_key_id      => "jutnxoxwwvzf266il65igf3fnjoq",
       :aws_secret_access_key  => "j3g5j3siqyclxefn7nvbgv4vthgfslkht23udjvsknh4imtff3pi2",
-      :region                 => 'us-east-1' # Change this for different AWS region. Default is 'us-east-1'
+      :region                 => 'us-east-1', # Change this for different AWS region. Default is 'us-east-1'
       :endpoint                 => 'https://gateway.storjshare.io' # Change this for different AWS region. Default is 'us-east-1'
   }
   config.fog_directory  = "hopify-bucket"
