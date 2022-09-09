92172e9ff970478139814231b2703d4692582e52
Khang Manh Nguyen <nguyenmanhkhang0805@gmail.com>
2022-09-09 07:12:11 +0700

    Modified initializers/s3.rb and config/storage.yml

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -3,9 +3,10 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
         :provider               => 'AWS',
-        :aws_access_key_id      => "jutnxoxwwvzf266il65igf3fnjoq",
-        :aws_secret_access_key  => "j3g5j3siqyclxefn7nvbgv4vthgfslkht23udjvsknh4imtff3pi2",
-        :region                 => 'us-east-1', # Change this for different AWS region. Default is 'us-east-1'
+        :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+        :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
+        :region                 => ENV['AWS_REGION'], # Change this for different AWS region. Default is 'us-east-1'
+        :bucket                 => ENV['AWS_BUCKET']
         :endpoint                 => 'https://gateway.storjshare.io' # Change this for different AWS region. Default is 'us-east-1'
     }
     config.fog_directory  = "hopify-bucket"
