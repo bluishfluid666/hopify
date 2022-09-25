8b2ff6006a1d737c2560896a66bc40b6de164ef1
Khang Manh Nguyen <62836167+bluishfluid666@users.noreply.github.com>
2022-09-25 16:04:34 +0700

    Delete 20220921045959_add_column_size_color_to_cart_items.rb

--- db/migrate/20220921045959_add_column_size_color_to_cart_items.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnSizeColorToCartItems < ActiveRecord::Migration[7.0]
-  def change
-    add_column :cart_items, :color, :string
-    add_reference :cart_items, :apparel_size
-  end
-end
