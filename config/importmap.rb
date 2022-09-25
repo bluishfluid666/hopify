04bb3a42de2d108033bda14ca2bc8894cefb0e6e
Khang Manh Nguyen <nguyenmanhkhang0805@gmail.com>
2022-09-25 21:06:04 +0700

    modify application.js and importmap.js

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,5 +6,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin_all_from "app/javascript/custom", under: "custom"
-pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.1/dist/jquery.js"
-pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.4/lib/assets/compiled/rails-ujs.js"
+# pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.1/dist/jquery.js"
+# pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.4/lib/assets/compiled/rails-ujs.js"
